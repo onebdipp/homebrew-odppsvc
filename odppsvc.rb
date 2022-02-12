11fde2acb86e125b40f1d6f8406772bcd3221411
onebdipp <99198319+onebdipp@users.noreply.github.com>
2022-02-12 11:20:44 -0500

    Update odppsvc.rb

--- odppsvc.rb
+++ odppsvc.rb
@@ -4,7 +4,7 @@ class Odppsvc < Formula
   homepage "bdiplus.com"
   url "https://github.com/onebdipp/od369/archive/refs/tags/v5.0.tar.gz"
   sha256 "122c0cf7472096ad85db358bc8929d64422a3670d94cce5cdcdd0bdc50933330"
-  license "MIT"
+  license ""
 
   # depends_on "cmake" => :build
 
@@ -15,7 +15,7 @@ class Odppsvc < Formula
     system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install "odppLink"
-    config.install "odppsvc.properties"
+    bin.install "odppsvc.properties"
     bin.install "odppsvc.jar"
   end
 
