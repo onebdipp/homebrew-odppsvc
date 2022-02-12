f3265ba8744d7a2a916120dc0e37a22295d8e595
onebdipp <99198319+onebdipp@users.noreply.github.com>
2022-02-12 15:41:31 -0500

    Update odppsvc.rb

--- odppsvc.rb
+++ odppsvc.rb
@@ -2,9 +2,9 @@ require 'formula'
 class Odppsvc < Formula
   desc "Odppsvc Description "
   homepage "bdiplus.com"
-  url "https://github.com/onebdipp/od369/archive/refs/tags/v5.0.tar.gz"
-  sha256 "122c0cf7472096ad85db358bc8929d64422a3670d94cce5cdcdd0bdc50933330"
-  license "4.0"
+  url "https://github.com/onebdipp/od369/archive/refs/tags/linuxv5.tar.gz"
+  sha256 "6341e83a3347c299099147ecc8582e27ba88eff3d816c29457b95f856dcd029b"
+  license "bdiplus-onedata-2022"
 
   # depends_on "cmake" => :build
   depends_on "bash"
@@ -12,7 +12,6 @@ class Odppsvc < Formula
   def install
 #    system "./configure", *std_configure_args, "--disable-silent-rules"
     bin.install "odppLink"
-    prefix.install Dir["config"]
     bin.install "odppsvc.properties"
     bin.install "odppsvc.jar"
   end
