a283020c9ca744a1bdb5847f299e07bd41cf0840
onebdipp <99198319+onebdipp@users.noreply.github.com>
2022-02-12 17:30:13 -0500

    Update odppsvc.rb

--- odppsvc.rb
+++ odppsvc.rb
@@ -2,8 +2,8 @@ require 'formula'
 class Odppsvc < Formula
   desc "Odppsvc Description "
   homepage "bdiplus.com"
-  url "https://github.com/onebdipp/od369/archive/refs/tags/odpp7.0.tar.gz"
-  sha256 "51630c49082a7f72fcfd307d12b302cc468017b1ee76529064fcc36cfeed1b2c"
+  url "https://github.com/onebdipp/od369/archive/refs/tags/odppsvc1.0.tar.gz"
+  sha256 "ba0d30209212b83e3b7e88f086691f2762cfb48c80a6acabaf193bd7d4d3dc28"
   license "bdiplus-onedata-2022"
 
   # depends_on "cmake" => :build
@@ -16,6 +16,7 @@ class Odppsvc < Formula
     bin.install "odppsvc"
     bin.install "config"
     bin.install "bin"
+    system "echo","If successfully installed run odppLink from your command line to link this platform with OneDATA.Plus environment"
   end
 
   test do
